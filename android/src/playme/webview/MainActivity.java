3c28a4e2f821f7504bc7cd4161b859a956ec8cb9
root <root@vmi2678268.contaboserver.net>
2026-07-19 07:49:01 +0200

    android: WebView APK source (abrir con Android Studio y Build)

---
+++ android/src/playme/webview/MainActivity.java
@@ -0,0 +1,35 @@
+package playme.webview;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends android.app.Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(false);
+        settings.setAllowContentAccess(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://95.111.238.114:8090");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
